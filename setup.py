82b610ed30589a6cdb95973e452f589fde697250
Karel Blavka <karel.blavka@bigclown.com>
2019-02-19 09:43:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,22 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
+
 setup(
     name='bcf',
-    packages=["bcf", "bcf.flasher", "bcf.flasher.serialport", "bcf.log", "bcf.repos", "bcf.firmware"],
+    packages=find_packages(),
     version='@@VERSION@@',
     description='BigClown Firmware Tool.',
     author='HARDWARIO s.r.o.',
     author_email='karel.blavka@bigclown.com',
     url='https://github.com/bigclownlabs/bch-firmware-tool',
     include_package_data=True,
-    install_requires=[
-        'appdirs>=1.4', 
-        'pyserial>=3.0', 
-        'colorama>=0.3', 
-        'PyYAML>=3.11', 
-        'schema>=0.6.7', 
-        'requests>=2.18', 
-        'Click>=6.0', 
-        'intelhex>=2.2.1'
-    ],
+    install_requires=requirements,
     license='MIT',
     zip_safe=False,
     keywords=['BigClown', 'bcf', 'firmware', 'flasher'],
@@ -39,7 +36,6 @@ setup(
         [console_scripts]
         bcf=bcf.cli:main
     ''',
-    long_description='''
-BigClown Firmware Tool.
-'''
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
