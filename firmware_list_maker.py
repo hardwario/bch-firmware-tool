ba685b271b8448307975363201dc6d909fc36751
Karel Blavka <karel.blavka@bigclown.com>
2020-09-21 08:23:16 +0200

    Update firmware_list_maker.py

--- firmware_list_maker.py
+++ firmware_list_maker.py
@@ -14,7 +14,7 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 ga = GithubApi(oauth_token=os.environ.get('GIT_OAUTH_TOKEN', None))
 
-# pprint(ga.make_firmware_list_from_repo("bigclownprojects/bcf-infra-grid-lcd-mirror")); exit()
+#pprint(ga.make_firmware_list_from_repo("bigclownprojects/bcf-radio-key-code")); exit()
 
 def sort_firmware_key(firmware):
     name = firmware['name']
@@ -23,10 +23,10 @@ def sort_firmware_key(firmware):
             return str(i) + name
     return name
 
-firmware_list = ga.make_firmware_list_for_owner("bigclownlabs")
+firmware_list = ga.make_firmware_list_for_owner("hardwario-kit")
 firmware_list.sort(key=sort_firmware_key)
 
-bigclownprojects_list = ga.make_firmware_list_for_owner("bigclownprojects")
+bigclownprojects_list = ga.make_firmware_list_for_owner("hardwario-projects")
 bigclownprojects_list.sort(key=sort_firmware_key)
 
 firmware_list += bigclownprojects_list
@@ -35,11 +35,14 @@ print ("=== sorted ===")
 
 for firmware in firmware_list:
     print(firmware['name'])
-    if firmware['name'] == "bigclownlabs/bcf-gateway-usb-dongle":
+    if firmware['name'] == "hardwario/bcf-gateway-usb-dongle":
         firmware["images"] = [{
             "title": "",
             "url": "https://cdn.myshoptet.com/usr/shop.bigclown.com/user/shop/detail_alt_1/159-2.png?5a1af76a"
         }]
+
+with open('bc-yml') as f:
+    firmware_list += yaml.safe_load(f)['list']
 
 payload = {
     "list": firmware_list,
